73f2ae1cbe6cae05fdae95eecb40edb06c21b223
Geoffrey Siele <sielekrisp@gmail.com>
2020-10-14 17:58:25 +0300

    About, Contact, Footer, and other static pages

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,43 +1,72 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
+// Utils
 import "./Footer.css";
+import * as ROUTES from "../../constants/routes";
 
 function Footer() {
   return (
-    <footer>
-      <h1 className="footer-header"><span className="farm-txt">Farm</span><span className="cart-txt">Cart</span></h1>
+    <footer style={{  marginTop: 25 }}>
+      <h1 className="footer-header">
+        <span className="farm-txt">Farm</span>
+        <span className="cart-txt">Cart</span>
+      </h1>
       <div className="footer-body">
         <div className="footer-column">
-          <h3>Resources</h3>
+          <h2 className="footer-subheader">Resources</h2>
           <div className="footer-item">Products & Services</div>
           <div className="footer-item">How-to guides</div>
           <div className="footer-item">Blog</div>
           <div className="footer-item">FAQ</div>
         </div>
+
         <div className="footer-column">
-          <h3>Legal</h3>
+          <h2 className="footer-subheader">Legal</h2>
+          <div className="footer-item">
+            <NavLink className="active" aria-current="page" to={ROUTES.PRIVACY}>
+              Privacy Policy
+            </NavLink>
+          </div>
+          <div className="footer-item">
+            <NavLink className="active" aria-current="page" to={ROUTES.TOS}>
+              Terms of Service
+            </NavLink>
+          </div>
+          <div className="footer-item">
+            <NavLink
+              className="active"
+              aria-current="page"
+              to={ROUTES.COOKIE_POLICY}
+            >
+              Cookie Policy
+            </NavLink>
+          </div>
           <div className="footer-item">Tax Compliance</div>
-          <div className="footer-item">Privacy Policy</div>
-          <div className="footer-item">Terms and Conditions</div>
-          <div className="footer-item">Cookie Policy</div>
         </div>
+
         <div className="footer-column">
-          <h3>Contacts</h3>
+          <h2 className="footer-subheader">Contacts</h2>
           <div className="footer-item">One</div>
           <div className="footer-item">Two</div>
           <div className="footer-item">Three</div>
           <div className="footer-item">Four</div>
         </div>
       </div>
-      <div style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        paddingTop: 20,
-        color: "orangered"
-      }}>&#x000A9;&nbsp;2020 FarmCart. All rights reserved.</div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          marginTop: 20,
+          paddingTop: 20,
+          color: "orangered",
+        }}
+      >
+        &#x000a9;&nbsp;2020 FarmCart. All rights reserved.
+      </div>
     </footer>
-  );
+  ); // return
 } // Footer
 
 export default Footer;
