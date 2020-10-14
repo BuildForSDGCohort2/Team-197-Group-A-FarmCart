afbf67bd24b08ae6de23db7fced041698102a731
Geoffrey Siele <sielekrisp@gmail.com>
2020-10-14 16:53:57 +0300

    Update Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 // Modules
 import React from "react";
 import { NavLink, useHistory } from "react-router-dom";
+
 import firebase from "firebase/app";
 import "firebase/auth";
 
@@ -8,26 +9,48 @@ import "firebase/auth";
 import "./Navbar.css";
 import * as ROUTES from "../../constants/routes";
 
+import CartService from "../../services/cart.service";
+
+// Material UI
+import { makeStyles } from "@material-ui/core/styles";
+
+// import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    margin: theme.spacing(1),
+    color: "#000",
+  },
+})); // useStyles
+
 function Navbar() {
+  const classes = useStyles();
   const history = useHistory();
   const user = JSON.parse(window.localStorage.getItem("user"));
+  const myCart = JSON.parse(window.localStorage.getItem("myCart"));
 
-  // const toggleLinks = () => {
-  //   const loggedOut = window.document.querySelectorAll(".logged-out");
-  //   const loggedIn = window.document.querySelectorAll(".logged-in");
-  //   if (user) {
-  //     loggedIn.forEach((link) => (link.style.display = "block"));
-  //     loggedOut.forEach((link) => (link.style.display = "none"));
-  //   } else {
-  //     loggedIn.forEach((link) => (link.style.display = "none"));
-  //     loggedOut.forEach((link) => (link.style.display = "block"));
-  //   }
-  // };
+  /**
+   * toggleLinks displays or hides some links based on whether
+   * the current user is logged in or not.
+   *
+   * @returns undefined
+   */
+  function toggleLinks() {
+    const loggedOut = window.document.querySelectorAll(".logged-out");
+    const loggedIn = window.document.querySelectorAll(".logged-in");
+    if (user) {
+      loggedIn.forEach((link) => (link.style.display = "block"));
+      loggedOut.forEach((link) => (link.style.display = "none"));
+    } else {
+      loggedIn.forEach((link) => (link.style.display = "none"));
+      loggedOut.forEach((link) => (link.style.display = "block"));
+    }
+  }
 
-  // toggleLinks();
+  toggleLinks();
 
   let displayPic;
-
   if (user && user.photoURL) {
     displayPic = (
       <img
@@ -62,85 +85,102 @@ function Navbar() {
   displayPic = user && displayPic ? displayPic : "";
 
   return (
-    <div>
-      <nav>
-        <ul className="navbar">
-          <li className="logged-out">
-            <NavLink className="active" aria-current="page" to={ROUTES.HOME}>
-              Home
-            </NavLink>
-          </li>
+    <>
+      <div className="nav-container">
+        <nav>
+          <ul className="navbar">
+            <li>
+              <NavLink className="active" aria-current="page" to={ROUTES.HOME}>
+                Home
+              </NavLink>
+            </li>
 
-          <li className="logged-in">
-            <NavLink
-              className="selected-link"
-              aria-current="page"
-              to={ROUTES.ADD_PRODUCT}
-            >
-              Add Product
-            </NavLink>
-          </li>
+            <li className="logged-in">
+              <NavLink
+                className="selected-link"
+                aria-current="page"
+                to={ROUTES.ADD_PRODUCT}
+              >
+                Add Product
+              </NavLink>
+            </li>
 
-          <li className="logged-out">
-            <NavLink
-              className="selected-link"
-              aria-current="page"
-              to={ROUTES.ABOUT}
-            >
-              About
-            </NavLink>
-          </li>
+            <li>
+              <NavLink
+                className="selected-link"
+                aria-current="page"
+                to={ROUTES.ABOUT}
+              >
+                About
+              </NavLink>
+            </li>
 
-          <li className="logged-out">
-            <NavLink to={ROUTES.CONTACT}>Contact</NavLink>
-          </li>
+            <li>
+              <NavLink to={ROUTES.CONTACT}>Contact</NavLink>
+            </li>
 
-          <li className="logged-out">
-            <button
-              style={{
-                backgroundColor: "#3ed715",
-                borderRadius: 5,
-                color: "#fff",
-              }}
-              onClick={() => {
-                return history.push("/sign-up");
-              }}
-            >
-              Sign In
-            </button>
-          </li>
+            <li className="logged-out">
+              <button
+                style={{
+                  backgroundColor: "#3ed715",
+                  borderRadius: 5,
+                  color: "#fff",
+                  cursor: "pointer",
+                }}
+                onClick={() => {
+                  return history.push("/sign-up");
+                }}
+              >
+                Sign In
+              </button>
+            </li>
 
-          <li className="logged-in">
-            <button
-              style={{
-                backgroundColor: "red",
-                borderRadius: 5,
-                color: "#fff",
-              }}
-              onClick={() => {
-                if (user !== null) {
-                  firebase.auth().signOut();
-                  alert("Logged out successfully.");
-                  window.localStorage.removeItem("myCart");
-                  window.localStorage.removeItem("user");
-                  history.push("/");
-                  return (() => {
-                    setTimeout(() => window.location.reload(), 2000);
-                  })();
-                } else {
-                  return;
-                }
-              }}
-            >
-              Log Out
-            </button>
-          </li>
-          <li id="display-pic" className="logged-in">
-            {displayPic}
-          </li>
-        </ul>
-      </nav>
-    </div>
+            <li className="logged-in">
+              <button
+                style={{
+                  backgroundColor: "red",
+                  borderRadius: 5,
+                  color: "#fff",
+                  cursor: "pointer",
+                }}
+                onClick={() => {
+                  if (user !== null) {
+                    firebase.auth().signOut();
+                    alert("Logged out successfully.");
+                    window.localStorage.removeItem("myCart");
+                    window.localStorage.removeItem("user");
+                    history.push("/");
+                    return (() => {
+                      setTimeout(() => window.location.reload(), 2000);
+                    })();
+                  } else {
+                    return;
+                  }
+                }}
+              >
+                Log Out
+              </button>
+            </li>
+          </ul>
+        </nav>
+        <div id="display-pic" className="logged-in">
+          {displayPic}
+        </div>
+        <div className="cart logged-in">
+          <div style={{ 
+            color: "#000",
+            fontSize: 13,
+            fontWeight: 700,
+            marginLeft: 15,
+            marginTop: 5,
+            marginBottom: -13,
+          }}>{CartService.getNumCartItems(myCart) ? CartService.getNumCartItems(myCart) : 0}</div>
+          <div>
+            <ShoppingCartOutlinedIcon fontSize="default" className={classes.avatar} />
+          </div>
+        </div>
+      </div>
+    </>
   ); // return
 } // Navbar
 
