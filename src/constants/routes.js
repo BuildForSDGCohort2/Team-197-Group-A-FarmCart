5252c9f293384c53d37ea8c194b358adb944cea8
Geoffrey Siele <sielekrisp@gmail.com>
2020-10-15 12:38:37 +0300

    Update routes file

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,33 +1,40 @@
+// Key:
+// RL ==> requires login
+// NL ==> no login required
+
+
 // Auth
-export const SIGN_IN = "/sign-in";
-export const SIGN_UP = "/sign-up";
+export const SIGN_UP = "/sign-up"; // NL
 
 // Users
-export const ADMIN = "/admin";
-export const PROFILE = "/profile";
-export const DASHBOARD = "/dashboard";
-export const EDIT_PROFILE = "/users/:id/profile/edit";
+export const ADMIN = "/admin";// RL
+export const PROFILE = "/profile";// RL 
+export const USER_PROFILE = "/users/:id/profile"; // NL
+export const DASHBOARD = "/dashboard";// RL
+export const EDIT_PROFILE = "/profile/edit";// RL
 
 // General
-export const HOME = "/";
-export const ABOUT = "/about";
-export const CONTACT = "/contact-us";
+export const HOME = "/"; // NL
+export const ABOUT = "/about"; // NL
+export const CONTACT = "/contact-us"; // NL
+export const TOS = "/tos"; // NL
+export const PRIVACY = "/privacy"; // NL
+export const COOKIE_POLICY = "/cookie-policy"; // NL
 
 // Products
-export const ADD_PRODUCT = "/products/add";
-export const PRODUCTS = "/products";
-export const USER_PRODUCTS = "/products/users/:id";
-export const PRODUCT_DETAILS = "/products/:id";
-export const EDIT_PRODUCT = "/products/:id/edit";
+export const ADD_PRODUCT = "/add-product";// RL
+export const PRODUCTS = "/products"; // NL
+export const PRODUCT_DETAILS = "/products/:id"; // NL
+export const USER_PRODUCTS = "/products/users/:id"; // NL
+export const EDIT_PRODUCT = "/products/:id/edit";// RL
 
 // Carts
-export const VIEW_CART = "/carts/:cartName";
-export const EDIT_CART = "/carts/:cartName/edit";
-export const CHECK_OUT = "/carts/:cartName/checkout";
+export const VIEW_CART = "/cart/view";// RL
+export const CHECK_OUT = "/cart/view/checkout";// RL
 
 // Posts
-export const POSTS = "/blog";
-export const ADD_POST = "/blog/add";
-export const EDIT_POST = "/blog/:id/edit";
-export const VIEW_POST = "/blog/:id";
-export const USER_POSTS = "/blog/users/:id";
+export const ADD_POST = "/blog/add";// RL
+export const POSTS = "/blog"; // NL
+export const VIEW_POST = "/blog/:id"; // NL
+export const USER_POSTS = "/blog/users/:id"; // NL
+export const EDIT_POST = "/blog/:id/edit"; // RL
